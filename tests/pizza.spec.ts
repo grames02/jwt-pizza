39b3f347f4104c8e47e5ad365b4db53ddc3a9899
grames02 <grames02@gmail.com>
2026-02-10 23:33:02 -0700

    I pass for 80% in my terminal, but my GitHub isn't passing for some reason. It fails 6 tests

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -180,6 +180,7 @@ test('Login as Franchisee and view stores.', async ({ page }) => {
   await page.getByLabel('Global').getByRole('link', { name: 'Franchise' }).click();
 });
 
+
 test('View Admin Page', async ({ page }) => {
 await page.goto('http://localhost:5173/');
 await page.getByRole('link', { name: 'Login' }).click();
@@ -257,4 +258,14 @@ test('Admin, close store', async ({ page }) => {
     await page.getByRole('button', { name: 'Login' }).click();
     await page.getByRole('link', { name: 'Admin' }).click();
     await page.getByRole('row', { name: 'Test123 0 ₿ Close' }).getByRole('button').click();
+});
+
+test('Login, and logout.', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByRole('link', { name: 'Logout' }).click();
 });
